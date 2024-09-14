d658c32c88a74266bac7229081970ad72af2f4bb
Pikaagante <briaultmaxence@gmail.com>
2024-09-15 00:59:50 +0200

    upate security

--- config/config.js
+++ config/config.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const connexion = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'pmove'
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = connexion;
