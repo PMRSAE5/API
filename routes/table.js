61cccb3021e5691cf42a69704b5c777f6068013f
Pikaagante <briaultmaxence@gmail.com>
2024-09-15 01:08:38 +0200

    add controller

--- routes/table.js
+++ routes/table.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const TableController = require('../controller/TableController');
 
 router.get("/", (req, res) => {
-  req.connexion.query('SELECT * FROM test', (err, rows) => {
+  TableController.getAllTests(req.connexion, (err, rows) => {
     if (err) {
       res.status(500).json({ error: 'Erreur lors de la récupération des données' });
       return;
@@ -12,15 +13,25 @@ router.get("/", (req, res) => {
 });
 
 router.post("/user", (req, res) => {
-    const { pseudo, num, mail } = req.body;
-    const query = 'INSERT INTO test (pseudo, num, mail) VALUES (?, ?, ?)';
-    req.connexion.query(query, [pseudo, num, mail], (err, result) => {
-      if (err) {
-        res.status(500).json({ error: 'Erreur lors de l\'insertion des données' });
-        return;
-      }
-      res.status(201).json({ message: 'Utilisateur ajouté avec succès', id: result.insertId });
-    });
+  const { pseudo, num, mail } = req.body;
+  TableController.insertUser(req.connexion, { pseudo, num, mail }, (err, result) => {
+    if (err) {
+      res.status(500).json({ error: 'Erreur lors de l\'insertion des données' });
+      return;
+    }
+    res.status(201).json({ message: 'Utilisateur ajouté avec succès', id: result.insertId });
   });
+});
+
+router.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+  TableController.getUserbyId(req.connexion, { id }, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: 'Erreur lors de la récupération des données' });
+      return;
+    }
+    res.status(200).json(rows);
+  });
+});
 
 module.exports = router;
