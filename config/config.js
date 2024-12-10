a8edd3392043ce8987c49db05f5de296d5a77b93
Pikaagante <briaultmaxence@gmail.com>
2024-12-10 15:53:10 +0100

    carameldansen LALALALa

--- config/config.js
+++ config/config.js
@@ -4,6 +4,7 @@ const mysql = require("mysql");
 require('dotenv').config();
 
 const connexion = mysql.createConnection({
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
